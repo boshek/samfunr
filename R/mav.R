cddda46e039a8590bf4a4855f919f242a74860ab
Sam Albers <sam.albers@gmail.com>
2017-01-10 09:22:20 -0800

    Added some other functions and updated documentation

---
+++ R/mav.R
@@ -0,0 +1,11 @@
+#' @title Moving average over a continuous variable
+#' @description Apply a moving average over a variable defaulting to a window of 5
+#'
+#' @param x Continuous variable over which to apply the moving average
+#' @param n number of observation over which to take the moving average
+#'
+#' @return The moving (or rolling) average of x over the n window
+#'
+#' @source http://druedin.com/2012/08/11/moving-averages-in-r/
+
+mav <- function(x,n=5){stats::filter(x,rep(1/n,n), sides=2)}
--- R/sum_stat.R
+++ R/sum_stat.R
@@ -1,2 +1,15 @@
-##Functions
+#' @title Standard Error
+#' @description Calculate the standard error of a distribution.
+#' @param x distribution over which to calculate the standard error
+#' @note the variance of the sample is calculate with na.rm=TRUE
+#' @examples
+#' x <- 1:10
+#' se(x=x)
 se <- function(x) sqrt(var(x, na.rm=TRUE)/length(x)) # note NA remove
+
+
+
+
+
+
+

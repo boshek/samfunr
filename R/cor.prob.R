12fd7172ab327b5b38e8cb9549104bee26a2cbc3
Sam Albers <sam.albers@gmail.com>
2017-05-03 13:12:58 -0700

    Add cor.prob

---
+++ R/cor.prob.R
@@ -0,0 +1,14 @@
+#' @export
+#' @title Correlation matrix
+#' @description Correlation Matrix with strength and significance
+#' @export
+#' @source https://stat.ethz.ch/pipermail/r-help/2001-November/016201.html
+
+cor.prob <- function(X, dfr = nrow(X) - 2) {
+  R <- cor(X)
+  above <- row(R) < col(R)
+  r2 <- R[above]^2
+  Fstat <- r2 * dfr / (1 - r2)
+  R[above] <- 1 - pf(Fstat, 1, dfr)
+  R
+}

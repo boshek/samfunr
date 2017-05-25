693b9f2d9f3ea54a70755ed97e3aa76e150d197a
Sam Albers <sam.albers@gmail.com>
2017-05-25 15:13:45 -0700

    no change; testing

--- R/cor.prob.R
+++ R/cor.prob.R
@@ -3,6 +3,9 @@
 #' @description Correlation Matrix with strength and significance
 #' @export
 #' @source https://stat.ethz.ch/pipermail/r-help/2001-November/016201.html
+#'
+#'
+#'
 
 cor.prob <- function(X, dfr = nrow(X) - 2) {
   R <- cor(X)
